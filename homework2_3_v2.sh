c0b003930bb8f63e47d58b08498e024cc2b94ce2
Water Chen <2399740787@qq.com>
2023-10-27 04:56:10 +0000

    bug fixed for homework2_3_v2

--- homework2_3_v2.sh
+++ homework2_3_v2.sh
@@ -5,11 +5,11 @@ do
 	if test -d ./$file
 	then
 		echo ""$file" is a dir."
-		echo $file > dirname2_3_v2.txt
+		echo $file >> dirname2_3_v2.txt
 	elif test -f ./$file
 	then
 		echo ""$file" is a file."
-		echo $file > filename2_3_v2.txt
+		echo $file >> filename2_3_v2.txt
 	else
 		echo "Filetype to be ditermined."
 	fi
