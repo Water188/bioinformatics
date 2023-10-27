6f3a912a4251a23fa3bedd470c3242f4d8476917
Water Chen <2399740787@qq.com>
2023-10-27 04:42:31 +0000

    renamed homework2.3.sh; homework v2.0

---
+++ homework2_3_v2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cd bash_homework
+for file in `ls`
+do
+	if test -d ./$file
+	then
+		echo ""$file" is a dir."
+		echo $file > dirname2_3_v2.txt
+	elif test -f ./$file
+	then
+		echo ""$file" is a file."
+		echo $file > filename2_3_v2.txt
+	else
+		echo "Filetype to be ditermined."
+	fi
+done
+mv dirname2_3_v2.txt ../homework_outcome
+mv filename2_3_v2.txt ../homework_outcome
+
