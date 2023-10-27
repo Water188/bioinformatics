b96c709c20bb4e71d2ea06bc0d5d77f7ba381cce
Water Chen <2399740787@qq.com>
2023-10-27 03:09:23 +0000

    homework done

--- homework2.3.sh
+++ homework2.3.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-
+# read dir info
+ls -al bash_homework > result2_3.txt
+sed -i '1,3d' result2_3.txt
+grep '^-' result2_3.txt | awk ' { print $9 } ' > filename2_3.txt
+grep '^d' result2_3.txt | awk ' { print $9 } ' > dirname2_3.txt
+mv filename2_3.txt homework_outcome
+mv dirname2_3.txt homework_outcome
+mv result2_3.txt homework_outcome
+echo "Done"
