c12e4482b8ca78724014d5f5b727f0047530bf12
Water Chen <2399740787@qq.com>
2023-11-07 17:30:32 +0800

    fix a bug

--- homework3_1.sh
+++ homework3_1.sh
@@ -1,1 +1,2 @@
-awk '$2 > 1 -a $6 < 0.05 { print $1, $2, $6 } ' wt.light.vs.dark.all.txt | cut -d ' ' -f 1 > result3_1.txt
+#!/bin/bash
+awk '$2 > 1 && $6 < 0.05 { print $1, $2, $6 } ' wt.light.vs.dark.all.txt | cut -d ' ' -f 1 > result3_1.txt
